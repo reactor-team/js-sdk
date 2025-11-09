721a388fc52ddaf9cd80dc63bdb65eb0985bcd49
Dere-Wah <derexcontact@gmail.com>
2025-11-09 22:48:23 +0000

    add demo using dynamic UI
    This PR adds a new frontend demo, installable via `npx create-reactor-app`. With this demo it's possible
    to automatically generate a UI for any Reactor Model ran LOCALLY. It makes use of the new `ReactorController` component, which automatically requires from the model the capabalities, which the model sends back as a JSON schema.
    which was added in version 1.0.17 of the JS SDK.
    
    This is especially useful for researchers development, as it allows to quickly test and debug a model without needing to code a full frontend demo.
    
    topic: add-dynamic-ui-demo-example-frontend
    reviewers: snow, bryce

---
+++ examples/dynamic/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Reactor Dynamic UI Demo",
+  description: "Reactor Dynamic UI Demo",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
